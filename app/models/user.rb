a578036c2c6d253d279f11a2e91ff6c190880a7d
rai <rai_0528@ezweb.ne.jp>
2023-08-26 00:44:39 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:username]  # 認証のキーをメールから名前へ
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+
+  validates :username, presence: true, length: { minimum: 2 }
+
+  attachment :profile_image
+
+  # nameを利用してログイン
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value", { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+end
